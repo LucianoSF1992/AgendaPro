51d64f6dbdf27fbface21fc0073d16c3fb8feb3b
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2026-03-14 10:20:11 -0300

    feat: adicionar acesso ao modulo de clientes no menu e dashboard

--- obj/Debug/net9.0/AgendaPro.AssemblyInfo.cs
+++ obj/Debug/net9.0/AgendaPro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgendaPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbf3b3621e216759005f9a5385e770eb37160bc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465e5cbfd2271983fbd92137feac2c1691193a12")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgendaPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgendaPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
